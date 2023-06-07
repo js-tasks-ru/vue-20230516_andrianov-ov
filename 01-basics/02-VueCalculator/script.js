f2ec6649c0a404245127a74e485e1fdbc95c1e2d
Oleg Andrianov <andrianov.ov@yandex.ru>
2023-06-07 19:27:27 +0300

    first and second exercise solved

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,33 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const app = createApp({
+  data() {
+    return {
+      firstNumber: 0,
+      secondNumber: 0,
+      output: 0,
+      sign: 'sum',
+    }
+  },
+  computed: {
+    operator() {
+      return this.sign.value;
+    },
+  },
+  methods: {
+    getCalculate() {
+      if (this.sign === 'sum') {
+        this.output = parseInt(this.firstNumber) + parseInt(this.secondNumber);
+      } else if (this.sign === 'subtract') {
+        this.output = this.firstNumber - this.secondNumber;
+      } else if (this.sign === 'multiply') {
+        this.output = this.firstNumber * this.secondNumber;
+      } else {
+        this.output = this.firstNumber / this.secondNumber;
+      }
+    }
+  }
+});
+
+const vm = app.mount("#app");
