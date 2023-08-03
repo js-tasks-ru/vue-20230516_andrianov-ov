1bd3f722a28a946dc7a81f0f04ba512f12dc10a7
Oleg Andrianov <andrianov.ov@yandex.ru>
2023-08-03 16:59:05 +0300

    done with 6 exercise and need help for 7th

--- 02-components/07-PageMeetup/components/PageMeetup.js
+++ 02-components/07-PageMeetup/components/PageMeetup.js
@@ -1,26 +1,62 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import { fetchMeetupById } from '../meetupService.js';
+//import MeetupView from '../../06-MeetupView/components/MeetupView.js';
+import { defineAsyncComponent } from 'vue';
 
 export default defineComponent({
   name: 'PageMeetup',
 
+  data() {
+    return {
+      meetup: {},
+      state: ''
+    }
+  },
+
   components: {
     UiAlert,
     UiContainer,
+    MeetupView: defineAsyncComponent(() => {
+      import('../../06-MeetupView/components/MeetupView.js')
+    })
+  },
+
+  props: {
+    meetupId: {
+      type: Number,
+      required: true
+    }
+  },
+
+  methods: {
+    fetchMeetupById() {
+      fetchMeetupById(this.meetupId).then(
+        (data) => {
+          console.log(data);
+          this.state = 'done';
+          return this.meetup = data;
+        },
+        (error) => {
+          return this.state = 'error';
+        }
+      );
+    }
   },
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
+      <MeetupView v-if="this.state === 'done'" :meetup="fetchMeetupById" />
 
-      <UiContainer>
+      <UiContainer v-else-if="this.state === 'error'">
+        <UiAlert>error</UiAlert>
+      </UiContainer>
+
+      <UiContainer v-else>
         <UiAlert>Загрузка...</UiAlert>
       </UiContainer>
 
-      <UiContainer>
-        <UiAlert>error</UiAlert>
-      </UiContainer>
+
     </div>`,
 });
